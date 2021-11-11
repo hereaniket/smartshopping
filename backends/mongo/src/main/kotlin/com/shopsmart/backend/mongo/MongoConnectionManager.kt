7a8fb92b4848edd0b9ebb93c3c29db08b8709e67
aniket pathak <aniketpathak1@gmail.com>
2021-11-10 20:35:00 -0700

    init commit123

--- backends/mongo/src/main/kotlin/com/shopsmart/backend/mongo/MongoConnectionManager.kt
+++ backends/mongo/src/main/kotlin/com/shopsmart/backend/mongo/MongoConnectionManager.kt
@@ -1,16 +1,13 @@
 package com.shopsmart.backend.mongo
 
+import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 
 object MongoConnectionManager {
 
-    fun ini(){
+    lateinit var mongoClient: MongoClient
+    fun init(): MongoClient{
         val connStr = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-
-        val mongoClient = MongoClients.create(connStr)
-        mongoClient.listDatabases().forEach {
-            print(it.toString())
-        }
-        print(mongoClient.toString())
+        return MongoClients.create(connStr)
     }
 }
--- backends/mongo/src/test/kotlin/com/shopsmart/backend/mongo/test/MongoConnectionManagerTest.kt
+++ backends/mongo/src/test/kotlin/com/shopsmart/backend/mongo/test/MongoConnectionManagerTest.kt
@@ -1,6 +1,10 @@
 package com.shopsmart.backend.mongo.test
 
 import com.shopsmart.backend.mongo.MongoConnectionManager
+import org.bson.Document
+import org.bson.conversions.Bson
+import org.bson.types.ObjectId
+import org.junit.Assert
 import org.junit.Test
 
 class MongoConnectionManagerTest {
@@ -8,6 +12,23 @@ class MongoConnectionManagerTest {
 
     @Test
     fun testMongoConnection(){
-        MongoConnectionManager.ini()
+        val mongoClient = MongoConnectionManager.init()
+        val mongoDb = mongoClient.getDatabase("shopsmart")
+        val mongoCollection = mongoDb.getCollection("sore")
+
+        val doc = Document()
+        doc.append("key1","value1")
+        doc.append("key2","value2")
+        doc.append("key3","value3")
+        mongoCollection.insertOne(doc)
+
+        val docs = mongoCollection.countDocuments()
+
+        val result = mongoCollection.find()
+            .filter(Document("key3","value3"))
+            .first()
+
+        Assert.assertNotNull(mongoClient)
+        mongoClient.close()
     }
 }
